93caaaf3728a9289c0615f0ad7c65196a7241df5
zeyuanxy <zeyuanxy@gmail.com>
2015-03-24 23:54:55 +0800

    add ThreadPool

---
+++ ThreadPool/join_threads.h
@@ -0,0 +1,14 @@
+class join_threads {
+	std::vector<std::thread>& threads;
+public:
+	explicit join_threads(std::vector<std::thread>& threads_):
+		threads(threads_) {
+	}
+
+	~join_threads() {
+		for (unsigned long i = 0; i < threads.size(); ++i) {
+			if (threads[i].joinable())
+				threads[i].join();
+		}
+	}
+};
